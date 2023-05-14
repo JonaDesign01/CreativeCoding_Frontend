17a61acae9d7fd98c8f9bcea7605bc88284031e7
Jona Peeters <jonamjp@gmail.com>
2023-05-14 16:10:46 +0200

    realbutton with frontend design

--- takepicture.js
+++ takepicture.js
@@ -13,7 +13,25 @@ navigator.mediaDevices
   .catch((error) => {
     console.error(error);
   });
+
 /*
+function startTimer() {
+  let countdown = 60; // 120 = 2 minutes in seconds
+  let timer = setInterval(function () {
+    countdown--;
+    let minutes = Math.floor(countdown / 60);
+    let seconds = countdown % 60;
+    document.querySelector('#cooldown').textContent =
+      minutes + ':' + (seconds < 10 ? '0' : '') + seconds; // Update the timer display
+    if (countdown <= 0) {
+      clearInterval(timer); // Stop the timer
+      document.querySelector('#cooldown').textContent = ''; // Clear the timer display
+    }
+  }, 1000); // Run the timer every second
+}
+
+let countdownTimer = null;
+
 var ws;
 
 function wsConnect() {
@@ -24,46 +42,74 @@ function wsConnect() {
   ws.onmessage = function (msg) {
     console.log(msg.data);
     if (msg.data === '1') {
-      console.log('fdgdfgsd');
-      let count = 3;
-      timer.innerText = count;
-      const countdown = setInterval(() => {
-        count--;
-        if (count > 0) {
-          timer.innerText = count;
-        } else {
-          clearInterval(countdown);
-          timer.innerText = '';
-          const canvas = document.createElement('canvas');
-          const context = canvas.getContext('2d');
-          const videoAspectRatio = video.videoWidth / video.videoHeight;
-          const canvasWidth = window.innerWidth;
-          const canvasHeight = canvasWidth / videoAspectRatio;
+      if (countdownTimer === null) {
+        // change timeout in nodered to 1 minute
+        //takephoto
+        let count = 3;
+        const message = document.getElementById('message');
+        timer.innerText = count;
+        const countdown = setInterval(() => {
+          count--;
+          if (count > 0) {
+            timer.innerText = count;
+          } else {
+            clearInterval(countdown);
+            timer.innerText = '';
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            const videoAspectRatio = video.videoWidth / video.videoHeight;
+            const canvasWidth = window.innerWidth;
+            const canvasHeight = canvasWidth / videoAspectRatio;
 
-          canvas.width = canvasWidth;
-          canvas.height = canvasHeight;
+            canvas.width = canvasWidth;
+            canvas.height = canvasHeight;
 
-          context.drawImage(video, 0, 0, canvasWidth, canvasHeight);
+            context.drawImage(video, 0, 0, canvasWidth, canvasHeight);
 
-          fetch('http://localhost:3000/uploadimage', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ image: canvas.toDataURL('image/jpeg') }),
-          }).catch((error) => {
-            console.error(error);
-            alert('Failed to upload image');
-          });
+            fetch('http://localhost:3000/uploadimage', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ image: canvas.toDataURL('image/jpeg') }),
+            }).catch((error) => {
+              console.error(error);
+              alert('Failed to upload image');
+            });
 
-          // take a screenshot
-          fetch('http://localhost:3000/screenshots', {
-            method: 'POST',
-          });
-        }
-      }, 1000);
+            // take a screenshot
+            fetch('http://localhost:3000/screenshots', {
+              method: 'POST',
+            });
+            message.textContent = 'Picture taken, thank you!';
+          }
+        }, 1000);
+
+        //cooldown
+        setTimeout(function () {
+          startTimer(); // Start the countdown timer and disable the button
+          countdownTimer = setTimeout(() => {
+            countdownTimer = null; // Reset the countdown timer
+            message.textContent = '';
+          }, 60000); // 120000 is 2min in ms
+          document.querySelector('.cs-loader').style.display = 'block';
+        }, 3000);
+      }
     }
   };
+
+  //loader none
+  const cooldownEl = document.querySelector('#cooldown');
+  const mijnStatus = document.querySelector('#status');
+  mijnStatus.textContent = 'Status: you are good to go!';
+  cooldownEl.addEventListener('DOMSubtreeModified', function () {
+    if (cooldownEl.textContent === '') {
+      document.querySelector('.cs-loader').style.display = 'none';
+      mijnStatus.textContent = 'Status: you are good to go!';
+    } else {
+      mijnStatus.textContent = 'Status: NFT making in progress... please wait';
+    }
+  });
 
   ws.onopen = function () {
     // update the status with the connection status
